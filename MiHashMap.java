416340756e7d4f863da8c1843f4a17aa08bddeb0
BuildTools <unconfigured@null.spigotmc.org>
2015-02-25 15:44:02 +0100

    primer commit

---
+++ MiHashMap.java
@@ -0,0 +1,49 @@
+
+/**
+ * Clase MiHashMap que recrea los métodos del HashMap sin el uso de la clase HashMap.
+ * 
+ * @author (Josu) 
+ * @version (25/02/2015)
+ */
+public class MiHashMap
+{
+    
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap() {
+
+    }
+    
+    public int put(String clave, int valor) {        
+        
+    }
+    
+    public int get(String clave) {
+       
+    }
+    
+    public boolean isEmpty() {
+       
+    }
+    
+    public int size() {
+
+    }
+    
+    public int remove(String clave) {
+
+    }
+    
+    public void clear() {
+
+    }
+    
+    public boolean containsKey(String clave) {
+
+    }
+    
+    public boolean containsValue(int valor) {
+
+    }
+}
